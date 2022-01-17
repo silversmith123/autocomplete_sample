ef7f8a5916cfb91e49fa7605ca42d36f638f62ad
mac <t.bridgestone.m@gmail.com>
2022-01-17 23:20:43 +0900

    vue-simple-suggestが動くように

---
+++ front/autocomplete_sample/src/components/HelloSuggest.vue
@@ -0,0 +1,34 @@
+<!-- Some component.vue -->
+<template>
+  <VueSimpleSuggest
+    v-model="chosen"
+    :list="simpleSuggestionList"
+    :filter-by-query="true">
+<!-- Filter by input text to only show the matching results -->
+  </VueSimpleSuggest>
+</template>
+
+<script>
+  import VueSimpleSuggest from 'vue-simple-suggest'
+  import 'vue-simple-suggest/dist/styles.css' // Optional CSS
+
+  export default {
+    components: {
+      VueSimpleSuggest
+    },
+    data() {
+      return {
+        chosen: ''
+      }
+    },
+    methods: {
+      simpleSuggestionList() {
+        return [
+          'Vue.js',
+          'React.js',
+          'Angular.js'
+        ]
+      }
+    }
+  }
+</script>

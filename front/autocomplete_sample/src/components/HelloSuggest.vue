21f81558badf097e685955c6dd44a07c72f82cff
mac <t.bridgestone.m@gmail.com>
2022-01-18 00:59:08 +0900

    jsonのデータに対応

--- front/autocomplete_sample/src/components/HelloSuggest.vue
+++ front/autocomplete_sample/src/components/HelloSuggest.vue
@@ -14,6 +14,9 @@
       </div>
       <hr>
     </div>
+    <div slot="suggestion-item" slot-scope="{ suggestion }">
+      <div>{{ suggestion.title }} img {{ suggestion.img }}</div>
+    </div>
   </VueSimpleSuggest>
 </template>
 
@@ -33,9 +36,9 @@
     methods: {
       simpleSuggestionList() {
         return [
-          'Vue.js',
-          'React.js',
-          'Angular.js'
+          {'id':1, 'title':'Vue.js', 'img':'https://jp.vuejs.org/images/logo.svg'},
+          {'id':2, 'title':'React.js', 'img':'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'},
+          {'id':3, 'title':'Angular.js', 'img':'https://angularjs.org/img/angularjs-for-header-only.svg'}
         ]
       }
     }

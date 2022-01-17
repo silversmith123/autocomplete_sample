e3e9c2330c0037b23ef15b61038ef3ad205ef64f
mac <t.bridgestone.m@gmail.com>
2022-01-18 01:19:08 +0900

    画像が表示されるように

--- front/autocomplete_sample/src/components/HelloSuggest.vue
+++ front/autocomplete_sample/src/components/HelloSuggest.vue
@@ -15,7 +15,8 @@
       <hr>
     </div>
     <div slot="suggestion-item" slot-scope="{ suggestion }">
-      <div>{{ suggestion.title }} img {{ suggestion.img }}</div>
+      <img :src="suggestion.img" width="30px"/>
+      <span>{{ suggestion.title }}</span>
     </div>
   </VueSimpleSuggest>
 </template>
@@ -36,9 +37,9 @@
     methods: {
       simpleSuggestionList() {
         return [
-          {'id':1, 'title':'Vue.js', 'img':'https://jp.vuejs.org/images/logo.svg'},
-          {'id':2, 'title':'React.js', 'img':'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'},
-          {'id':3, 'title':'Angular.js', 'img':'https://angularjs.org/img/angularjs-for-header-only.svg'}
+          {'id':1, 'title':'Vue.js', 'img':'/img/logo.png'},
+          {'id':2, 'title':'React.js', 'img':'/img/logo.png'},
+          {'id':3, 'title':'Angular.js', 'img':'/img/logo.png'}
         ]
       }
     }

0620f93f10da5f257f6d0ab1e9b80e191155738d
mac <t.bridgestone.m@gmail.com>
2022-01-23 07:24:01 +0900

    esにsampleを入れらるようにした

--- backend/init_index.py
+++ backend/init_index.py
@@ -8,3 +8,8 @@ with open('my_suggest.json') as f:
 
 es.indices.create('my_suggest', body=configuration)
 
+with open('sample.json') as f:
+  sample = json.load(f)
+
+for doc in sample:
+  es.create('my_suggest',doc.pop('id'), doc)

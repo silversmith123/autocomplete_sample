e2a2481aae106e3d241b68b68102e2fd705cf672
mac <t.bridgestone.m@gmail.com>
2022-01-26 21:09:30 +0900

    warmingを増やす

--- backend/warming.sh
+++ backend/warming.sh
@@ -3,5 +3,14 @@ do
  curl "localhost/suggest?text=ソファ"
  curl "localhost/suggest?text=デスク"
  curl "localhost/suggest?text=ほんだな"
+ curl "localhost/suggest?text=sofa"
+ curl "localhost/suggest?text=desk"
+ curl "localhost/suggest?text=hon"
+ curl "localhost/suggest?text=ソファ"
+ curl "localhost/suggest?text=デスク"
+ curl "localhost/suggest?text=ほんだな"
+ curl "localhost/suggest?text=sofa"
+ curl "localhost/suggest?text=desk"
+ curl "localhost/suggest?text=hon"
  sleep 1
 done

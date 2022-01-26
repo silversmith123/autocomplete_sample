cce9e1e37b3b7d386070084ca6f3d8cb242c93fe
mac <t.bridgestone.m@gmail.com>
2022-01-27 04:21:51 +0900

    url修正

--- backend/warming.py
+++ backend/warming.py
@@ -4,5 +4,5 @@ es = Elasticsearch('http://localhost:9200')
 search_words = ['ソファ', 'PC', 'パソコン', 'デスク', 'ほんだな']
 for w in search_words:
   for token in es.indices.analyze({"analyzer":"readingform_index_analyzer", "text":w}, "my_suggest")['tokens']:
-    req = requests.get('http://localhost', params={'text': token['token']})
+    req = requests.get('http://localhost/suggest', params={'text': token['token']})
     print(req.text)
